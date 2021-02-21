afeb6686f9eeb125be520ba1eaa6edc9a1c06939
asyncdeveloper <contact@asyncdeveloper.com>
2021-02-21 01:20:47 +0100

    Add pagination

--- src/components/SearchPage.js
+++ src/components/SearchPage.js
@@ -3,12 +3,19 @@ import { Button, Card, Col, Container, Row } from "react-bootstrap";
 import { withRouter } from 'react-router-dom';
 import  bookImage from '../default-book.jpg';
 import SpinnerButton from '../common/SpinnerButton';
+import Pagination from 'react-js-pagination';
+import '../App.css';
 
 const SearchPage = (props) => {
+
+    const handlePageChange = (pageNumber) => setCurrentPage(pageNumber) ;
+
     const [searchTerm] = useState(props.match.params.query);
 
+    const dataPerPage = 10;
     const [responseData, setResponseData] = React.useState([]);
     const [isLoading, setIsLoading] = React.useState(true);
+    const [ activePage, setCurrentPage ] = useState(1);
 
     useEffect(() => {
         if (searchTerm == null || searchTerm === ''){
@@ -24,7 +31,10 @@ const SearchPage = (props) => {
             });
     }, [ searchTerm ]);
 
-    console.log(responseData);
+    // Logic for displaying current todos
+    const indexOfLastData  = activePage * dataPerPage;
+    const indexOfFirstData = indexOfLastData - dataPerPage;
+    const currentData      = responseData.slice( indexOfFirstData, indexOfLastData );
 
     return (
         <Container fluid className="px-4">
@@ -39,22 +49,36 @@ const SearchPage = (props) => {
                     ? <SpinnerButton />
                     : responseData.length === 0
                         ? <h2> No Result Found</h2>
-                        : responseData.map((row, index) => {
+                        : currentData.map((row, index) => {
                             const coverImage = row.cover_i !== undefined
                                 ? `http://covers.openlibrary.org/b/ID/${row.cover_i}-M.jpg`
                                 : bookImage ;
 
-                            return (<Card key={index} style={{width: '18rem'}} className="mx-2 my-1">
+                            return (
+                                <Card key={index} style={{width: '18rem'}} className="mx-2 my-1">
                                 <Card.Img variant="top" src={coverImage} style={{ height: '300px' }}  />
                                 <Card.Body>
                                     <Card.Title>{row.title}</Card.Title>
-                                    <Card.Text> Publish Year : {row.publish_year }</Card.Text>
                                     <Button variant="primary">View Book</Button>
                                 </Card.Body>
                             </Card>)
                     })
                 }
             </Row>
+            {
+                !isLoading && responseData.length > 0
+                    ? <Row>
+                        <div className="pagination">
+                            <Pagination
+                                activePage={ activePage }
+                                itemsCountPerPage={ dataPerPage }
+                                totalItemsCount={ responseData.length }
+                                pageRangeDisplayed={ dataPerPage }
+                                onChange={ handlePageChange }
+                            />
+                        </div>
+                    </Row> : null
+                }
         </Container>
     );
 }
